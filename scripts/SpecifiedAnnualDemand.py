37d688a9f08d81e989609abc4bd6c8bef133b2d4
Trevor Barnes <trevor_barnes@sfu.ca>
2021-05-06 12:43:53 -0700

    Added demand sources and python scripts to generate demand csvs

---
+++ scripts/SpecifiedAnnualDemand.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import os
+import numpy as np
+import datetime
+
+def main():
+    # PURPOSE: Creates otoole formatted SpecifiedAnnualDemand csv file 
+    # INPUT: none
+    # OUTPUT: none
+
+    ###########################################
+    # Model Parameters
+    ###########################################
+
+    #Dictionary holds Provice to Region mapping 
+    regions = {
+        'W':['BC','AB'],
+        'MW':['SAS','MAN'],
+        'ME':['ONT','NB'],
+        'E':['QC','NS','PEI','NL']}
+
+    #Years to Print over
+    years = range(2019,2051,1)
+
+    ###########################################
+    # Calculate demand  
+    ###########################################
+
+    #Read in csv containing data
+    sourceFile = '../dataSources/ProvincialAnnualDemand.csv'
+    df = pd.read_csv(sourceFile, index_col=0)
+
+    #Master list to output
+    #Region, fuel, year, value
+    demand = []
+    
+    for region, provinces in regions.items(): 
+        dfRegion = df[regions[region]]
+        sumDemand = dfRegion.loc[:,:].sum(axis=1)
+        for year in years:
+            demand.append([region, 'ELC', year, sumDemand[year]])
+
+    ###########################################
+    # Writing Demand Files 
+    ###########################################
+
+    df = pd.DataFrame(demand, columns = ['REGION', 'FUEL', 'YEAR', 'VALUE'])
+    df.to_csv('../src/data/SpecifiedAnnualDemand.csv', index=False)
+
+if __name__ == "__main__":
+    main()
