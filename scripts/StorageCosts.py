5cc0e89c35731b2f39c7730447e7e723b83456d2
KH <khenkelm@sfu.ca>
2021-10-22 15:03:21 -0700

    removed techlists_autogenerated

--- scripts/StorageCosts.py
+++ scripts/StorageCosts.py
@@ -17,14 +17,11 @@ def main():
     subregions = functions.openYaml().get('subregions_list')
     years = functions.getYears()
 
-    #Read in master list of technologies and get storage names
-    dfGeneration_raw = pd.read_csv('../dataSources/techList_AUTO_GENERATED.csv')
-    dfGeneration = dfGeneration_raw.loc[dfGeneration_raw['GENERATION'] == 'STO']
-    storages = dfGeneration['VALUE'].tolist()
-    if not storages:
-        df = pd.DataFrame(columns=['REGION','STORAGE','YEAR','VALUE'])
-        df.to_csv('../src/data/CapitalCostStorage.csv', index=False)
-        return
+    # Here, we would read in master list of technologies and get storage names, but none exist yet
+    # if not storages:
+    df = pd.DataFrame(columns=['REGION','STORAGE','YEAR','VALUE'])
+    df.to_csv('../src/data/CapitalCostStorage.csv', index=False)
+    return
 
     #Dictory to hold storage ype and cost in M$/GW
     storageCosts = {'TNK':11.673152}
